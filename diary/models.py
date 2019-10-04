fc69e4c9f59ef9a1eb0792c33f75416e7050e59a
chris <christianwilliamsjire@gmail.com>
2019-10-04 15:28:28 +0300

    This commit will add save function and search function on location module

--- diary/models.py
+++ diary/models.py
@@ -25,19 +25,35 @@ class Images(models.Model):
     location = models.ForeignKey(Location)
     category = models.ForeignKey(Category)
     
-    def save_Images(self):
+    def save_image(self):
         self.save()
         
-    def delete_Images(self):
+    def delete_image(self):
         self.delete()
-
+    
+    @classmethod    
+    def get_image(cls):
+        images = cls.objects.all()
+        return images
+    
+    
+    
+    
     
     
 class Location(models.Model):
     location = models.CharField(max_length= 20)
     
+    def save_location(self):
+        self.save()
+    
+    @classmethod
+    def search_by_location(cls,search_term):
+        results= cls.objects.filter(title__icontains = search_term)
+        return results
+    
     def __str__(self):
-        return self.name
+        return self.location
     
 class Category(models.Model):
     name = models.CharField(max_length= 20)
