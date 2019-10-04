78f2f8ba14708d09d84c123a44e5f23181543933
chris <christianwilliamsjire@gmail.com>
2019-10-04 11:32:13 +0300

    This commit will add base.html boiler plate

--- gallery/settings.py
+++ gallery/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'diary.apps.NewConfig'
+    'diary.apps.DiaryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
