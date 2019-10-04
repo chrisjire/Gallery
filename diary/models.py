4a500f001eae3f386a75b0e4e454bc8c332614f8
chris <christianwilliamsjire@gmail.com>
2019-10-04 15:30:04 +0300

    This commit will add save function to category  module

--- diary/models.py
+++ diary/models.py
@@ -56,7 +56,10 @@ class Location(models.Model):
         return self.location
     
 class Category(models.Model):
-    name = models.CharField(max_length= 20)
+    category = models.CharField(max_length= 20)
+    
+    def save_category(self):
+        self.save()
     
     def __str__(self):
         return self.name
