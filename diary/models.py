44da0c14b999780b564911f532c51308fe4efa84
chris <christianwilliamsjire@gmail.com>
2019-10-04 14:31:44 +0300

    This commit will add models for Editor, Tags And Images

--- diary/models.py
+++ diary/models.py
@@ -4,3 +4,22 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Editor(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField()
+
+class tags(models.Model):
+    name = models.CharField(max_length= 20)
+    
+    def __str__(self):
+        return self.name
+
+class Images(models.Model):
+    image = models.ImageField(upload_to = 'articles/')
+    image_name = models.TextField(max_length=50)
+    description = models.TextField()
+    editor = models.ForeignKey(Editor)
+    tags = models.ManyToManyField(tags)
+    
+    
--- gallery/settings.py
+++ gallery/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'diary.apps.DiaryConfig',
-    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -64,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -125,3 +125,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
