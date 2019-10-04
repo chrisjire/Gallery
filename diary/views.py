1e81a9e40f21e7d64aeb1ceb7a1c96a41af890cc
chris <christianwilliamsjire@gmail.com>
2019-10-04 10:55:47 +0300

    This commit will add templates folder and index.html file to diary app

--- diary/views.py
+++ diary/views.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime as dt
 
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome amigos')
+
